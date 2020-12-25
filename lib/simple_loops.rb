cfbb5ab9a351269801b3655cbd6cd866e968e96f
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 03:17:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,3 +4,12 @@ def loop_message_five_times(string)
     puts string
   }
 end
+
+counter = 0
+def loop_message_n_times(string, number)
+  while counter < number do
+    puts string
+  end
+end
+
+def 
