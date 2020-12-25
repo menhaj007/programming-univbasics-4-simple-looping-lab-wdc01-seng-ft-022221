7b2371d7de172ccc7a30da4aa21d719a9a8e1308
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2020-12-25 03:27:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,12 +25,13 @@ end
 
 def return_string_array(array)
   count = 0
-  stringArray = ""
-  while count < array.length do 
-    stringArray.push(array[count].to_s)
+  # stringArray = ""
+  while count < array.length do
+    array[count].to_s
+    # stringArray.push(array[count].to_s)
     count += 1
   end
-  stringArray
+  # stringArray
 end
 
   
